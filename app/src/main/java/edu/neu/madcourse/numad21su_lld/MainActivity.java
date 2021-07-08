ffa828b82b47a47a08b82e7d73f3ccf6e196dc91
lukaskatko <lukaskatko@gmail.com>
2021-07-08 19:13:01 -0400

    test Firebase db

--- app/src/main/java/edu/neu/madcourse/numad21su_lld/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_lld/MainActivity.java
@@ -1,14 +1,40 @@
 package edu.neu.madcourse.numad21su_lld;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText userNameText;
+    private String userName;
+    private DatabaseReference myDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+
+        userNameText = findViewById(R.id.enter_username);
+
+        //myDatabase = FirebaseDatabase.getInstance().getReference();
+
+        Button loginButton = findViewById(R.id.button);
+        loginButton.setOnClickListener(v -> {
+            userName = userNameText.getText().toString();
+            //myDatabase.child("Users").setValue("test");
+            //myDatabase.child("users").child("userName").setValue(userName);
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("message");
+            System.out.println("WTF");
+            myRef.setValue("Hello, World!");
+
+        });
     }
 }
