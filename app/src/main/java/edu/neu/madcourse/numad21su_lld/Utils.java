bf4111e5137d84db719093cf989582316e147177
Bessette <lbbesse@88665a29e198.ant.amazon.com>
2021-07-09 13:34:00 -0400

    looked at api for fcm and its a legacy http api, so maybe topic messaging is correct?

--- app/src/main/java/edu/neu/madcourse/numad21su_lld/Utils.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_lld/Utils.java
@@ -1,25 +1,51 @@
 package edu.neu.madcourse.numad21su_lld;
 
-import android.content.Context;
-import android.content.res.AssetManager;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Utils {
-    public static Properties getProperties(Context context)  {
-        Properties properties = new Properties();
-        AssetManager assetManager = context.getAssets();
-        InputStream inputStream = null;
+
+    public static String stickerHttpConnection(String serverToken, JSONObject jsonObject) {
         try {
-            inputStream = assetManager.open("firebase.properties");
-            properties.load(inputStream);
+            URL url = new URL("https://fcm.googleapis.com/fcm/send");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Authorization", serverToken);
+            conn.setDoOutput(true);
+
+            OutputStream outputStream = conn.getOutputStream();
+            outputStream.write(jsonObject.toString().getBytes());
+            outputStream.close();
+
+            InputStream inputStream = conn.getInputStream();
+            return convertStreamToString(inputStream);
         } catch (IOException e) {
-            e.printStackTrace();
+            return "NULL";
         }
 
-        return properties;
     }
 
+    public static String convertStreamToString(InputStream inputStream) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String len;
+            while ((len = bufferedReader.readLine()) != null) {
+                stringBuilder.append(len);
+            }
+            bufferedReader.close();
+            return stringBuilder.toString().replace(",", ",\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
