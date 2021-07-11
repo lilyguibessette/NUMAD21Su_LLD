d5618df61b900dfa170159e822b0c20790317315
lukaskatko <lukaskatko@gmail.com>
2021-07-10 20:05:49 -0400

    attempting FCM server

--- app/src/main/java/edu/neu/madcourse/numad21su_lld/ReceivedActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_lld/ReceivedActivity.java
@@ -2,8 +2,10 @@ package edu.neu.madcourse.numad21su_lld;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -21,6 +23,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -38,6 +41,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -616,6 +621,34 @@ public class ReceivedActivity extends AppCompatActivity implements SendStickerDi
         allUsersRef.addChildEventListener(validatedUsersListener);
     }
 
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param remoteMessageNotification FCM message  received.
+     */
+    private void showNotification(RemoteMessage.Notification remoteMessageNotification) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Notification notification;
+        NotificationCompat.Builder builder;
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+
+        builder = new NotificationCompat.Builder(this, "Muncha_Crunch_Channel");
+        notification = builder.setContentTitle(remoteMessageNotification.getTitle())
+                .setContentText(remoteMessageNotification.getBody())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notificationManager.notify(0, notification);
+
+    }
 }
 
 
